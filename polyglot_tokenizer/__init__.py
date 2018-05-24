5f3eadda5059b999afb0d3f7b4c520e2287f3e7e
irshadbhat <bhatirshad127@gmail.com>
2018-05-24 07:55:06 +0530

    added support for more languages

--- polyglot_tokenizer/__init__.py
+++ polyglot_tokenizer/__init__.py
@@ -29,8 +29,8 @@ def parse_args(args):
     languages = '''hi ur bn as gu ml pa te ta kn or mr cu myv
                 ne bo br ks en es ca cs de el en fi da eu kok
                 fr ga hu is it lt lv nl pl pt ro ru sk bm yue
-                sl sv zh et fo gl hsb af ar be hy bg ckb ka
-                kk ky la no fa uk tl tr vi yo ko'''.split()
+                sl sv zh et fo gl hsb af ar be hy bg ka ug hr
+                kk ky la no fa uk tl tr vi yo ko got ckb he id sr'''.split()
     lang_help = 'select language (2 letter ISO-639 code) {%s}' % (
                 ', '.join(languages))
     # parse command line arguments
